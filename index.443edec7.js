5b67f9ddce830b20d51cbd253bbb2185a306dcd9
Oleksii Karpov <alneon1@gmail.com>
2024-10-05 10:38:09 +0300

    make build

---
+++ index.443edec7.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__large-img");document.querySelector(".gallery__list").addEventListener("click",function(t){var r=t.target.closest(".list-item__link");r&&(e.src=r.href,t.preventDefault())});
+//# sourceMappingURL=index.443edec7.js.map
